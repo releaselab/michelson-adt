47160fdf24d7988429c965e4e985e5bcdf9f9fe4
Mário <mjp.pereira@fct.unl.pt>
2019-12-03 23:50:22 +0000

    Michelson -> WhyML plugin (wip): first baby steps

--- why3_plugin/why3_michelson.ml
+++ why3_plugin/why3_michelson.ml
@@ -1,14 +1,7 @@
-open Why3
-open Ptree
-open Michelson
-open Adt
+(* open Why3
+ * open Ptree
+ * open Michelson
+ * open Adt *)
 open Why3_michelson_trans
 
-let () = ignore (id)
-
-let () =
-  ignore (Qident {id_str = "ola"; id_ats = []; id_loc = Loc.dummy_position})
-
-let () = ignore (t_int)
-
-let () = ignore (T_int)
+let f x = inst x
--- why3_plugin/why3_michelson_trans.ml
+++ why3_plugin/why3_michelson_trans.ml
@@ -2,8 +2,23 @@ open Why3
 open Michelson
 open Adt
 open Ptree
+open Number
 
-let id =
-  Qident {id_str = "ola"; id_ats = []; id_loc = Loc.dummy_position}
+let mk_id id_str =
+  { id_str; id_ats = []; id_loc = Loc.dummy_position }
 
-let t_int = T_int
+let mk_expr expr_desc =
+  { expr_desc; expr_loc = Loc.dummy_position }
+
+let q_stack = Qident (mk_id "__stack__")
+
+let e_stack = mk_expr (Eident q_stack)
+
+let inst = function
+  | I_drop     -> Eidapp (Qident (mk_id "drop"), [e_stack])
+  | I_drop_n n -> let n = Z.to_string n in
+      let n = int_literal ILitDec ~neg:false (Lexlib.remove_underscores n) in
+      let n = mk_expr (Econst (Constant.ConstInt n)) in
+      Eidapp (Qident (mk_id "drop_n"), [e_stack; n])
+  | I_swap -> Eidapp (Qident (mk_id "swap"), [e_stack])
+  | _ -> assert false (* TODO *)
